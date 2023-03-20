102ae114f2c3cb53b956279b116296a715cc3bd1
johan <t511069160@qq.com>
2023-03-20 17:23:55 +0800

    只有admin用户才能登录后台

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
     def index
         flash[:alert] = "早安！你好！"
     end
+
+    def admin_required 
+        if !current_user.admin?
+            redirect_to "/", alert: "你不是管理员"
+        end
+    end
 end
