c58efac8021da7c0900e4c8200cc18f4996e4761
johan <t511069160@qq.com>
2023-03-20 14:11:48 +0800

    flash功能但是没有完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def index
+        flash[:alert] = "早安！你好！"
+    end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+var alertList = document.querySelectorAll('.alert')
+console.log('alertList', alertList)
+var alerts =  [].slice.call(alertList).map(function (element) {
+  return new bootstrap.Alert(element)
+});
