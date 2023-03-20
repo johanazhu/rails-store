34ce900d0b7a371997521c43d342768d1045a6b7
johan <t511069160@qq.com>
2023-03-20 17:48:02 +0800

    完成product页面

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "products#index"
 
   namespace :admin do
     resources :products
   end
 
+  resources :products
 
 end
