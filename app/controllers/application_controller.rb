fdaaa2d1e36f81f5cf6fa012e101aa00650dbb64
johan <t511069160@qq.com>
2023-03-21 15:48:17 +0800

    纠正一些错误

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,19 @@ class ApplicationController < ActionController::Base
         end
     end
 
-    helper_method :current_user
+    helper_method :current_cart
 
-    def current_user
-        @current_user ||= find_cart
+    def current_cart
+        @current_cart ||= find_cart
     end
 
-    def find_cart
-        cart = Cart.find_by(id: session[:cart_id])
-        if cart.black?
-            cart = Cart.create
+    private
+        def find_cart
+            cart = Cart.find_by(id: session[:cart_id])
+            if cart.blank?
+                cart = Cart.create
+            end
+            session[:cart_id] = cart.id
+            return cart
         end
-        session[:cart_id] = cart.id
-        return cart
-    end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
     def add_to_cart 
         @product = Product.find(params[:id])
 
-        current_user.add_production_to_cart(@product)
+        current_cart.add_product_to_cart(@product)
         flash[:notice] = "成功加入购物车"
         redirect_back(fallback_location: root_path)
     end
