dab81ca333b34094f3a8a3cbaea8803ad6acc5f6
johan <t511069160@qq.com>
2023-03-21 14:46:13 +0800

    fix加入购物车

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,8 @@ class ProductsController < ApplicationController
     
     def add_to_cart 
         @product = Product.find(params[:id])
-        redirect_to :back
+        # redirect_to :back
+        redirect_back(fallback_location: root_path)
         flash[:notice] = "已加入购物车"
     end
 end
