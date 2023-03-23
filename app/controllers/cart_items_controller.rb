9f076b7bec2ea3b761980a0c363c9b0f2898094d
johan <t511069160@qq.com>
2023-03-23 10:00:34 +0800

    购物车可选择商品数量

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,4 +11,15 @@ class CartItemsController < ApplicationController
         redirect_back(fallback_location: root_path)
     end
 
+    def update
+        @cart = current_cart
+        @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+        @cart_item.update(cart_item_params)
+        redirect_to carts_path
+    end
+
+    def cart_item_params
+        params.require(:cart_item).permit(:quantity)
+    end
+
 end
