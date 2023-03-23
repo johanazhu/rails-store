d0fee3776c81e77a30168ca49e3e162fb63b7d9f
johan <t511069160@qq.com>
2023-03-23 13:21:43 +0800

    订单id乱序

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_032339) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_051054) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
     t.integer "product_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_032339) do
     t.string "shipping_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
   create_table "product_lists", force: :cascade do |t|
