188a9b30dd1f8eb0cbd46740df9995cdfc55d2d6
johan <t511069160@qq.com>
2023-03-23 13:41:22 +0800

    结账

---
+++ db/migrate/20230323053114_add_is_paid_to_order.rb
@@ -0,0 +1,5 @@
+class AddIsPaidToOrder < ActiveRecord::Migration[7.0]
+  def change
+    add_column :orders, :is_paid, :boolean, default: false
+  end
+end
---
+++ db/migrate/20230323053248_add_payment_method_to_order.rb
@@ -0,0 +1,5 @@
+class AddPaymentMethodToOrder < ActiveRecord::Migration[7.0]
+  def change
+    add_column :orders, :payment_method, :string
+  end
+end
